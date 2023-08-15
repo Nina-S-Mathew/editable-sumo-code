4a6fb522a381aa6b548f3fd9a90c2491a9ff1488
Nina-S-Mathew <99273767+Nina-S-Mathew@users.noreply.github.com>
2023-08-15 14:03:05 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,35 @@
 radio.onReceivedValueDeprecated(function (name, value) {
-    if (name == "Seven") {
+    if (name == "Nineteen") {
         motion.turnLeft(value)
-    } else if (name == "Eight") {
+    } else if (name == "Twenty") {
         motion.turnRight(value)
-    } else if (name == "Nine") {
+    } else if (name == "Twenty One") {
         motion.turnRight(value)
-    } else if (name == "Ten") {
+    } else if (name == "Twenty Two") {
         motion.driveStraight(value)
-    } else if (name == "Eleven") {
+    } else if (name == "Twenty Three") {
         motion.driveStraight(value)
-    } else if (name == "Twelve") {
-        motion.drive(value, -1 * value)
+    } else if (name == "Twenty Four") {
+        motion.stop()
     }
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("Seven", 60)
+    radio.sendValue("Nineteen", 60)
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendValue("Eleven", 40)
+    radio.sendValue("Twenty Three", 40)
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendValue("Twelve", 40)
+    radio.sendValue("Twenty Four", 40)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendValue("Eight", 40)
+    radio.sendValue("Twenty", 40)
 })
 input.onGesture(Gesture.Shake, function () {
-    radio.sendValue("Nine", 40)
+    radio.sendValue("Twenty One", 40)
 })
 input.onGesture(Gesture.TiltRight, function () {
-    radio.sendValue("Ten", 40)
+    radio.sendValue("Twenty Two", 40)
 })
 basic.showIcon(IconNames.Angry)
 radio.setGroup(1)
